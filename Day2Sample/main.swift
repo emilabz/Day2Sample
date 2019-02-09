f879e79d64d67ec576b501f8085dccea6aa14441
MacStudent <macstudent@126-imac32.local>
2019-02-08 19:37:13 -0500

    array handling n slicing

--- Day2Sample/main.swift
+++ Day2Sample/main.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 print("Hello, World!")
-
+/*
 var a:Bool
 var s:String = "Hello"
 var str = String()
@@ -49,11 +49,42 @@ if(y.contains(-29))
 {
     print("TRUE")
 }
-/*for i in y
+for i in y
 {
     print(i)
     if(i==20)
     {
         break;
     }
-}*/
+}
+*/
+//Arrays
+var a = [10,20,30,40,50]
+for i in a
+{
+    print(i)
+}
+print(a[0],a[1],a[2],a[3],a[4])
+//print(a[5])
+var b = [Int]()
+b.append(100)
+b.append(200)
+b.append(300)
+//b[0] = 100
+print(b[0],b[1],b[2]);
+b += [111,222,333]
+let c = a + b
+print("C array contains")
+for i in c
+{
+    print(i)
+}
+print("C[1...3] contains")
+for i in c[1...3]
+{
+    print(i)
+}
+print("C[0] contains ",c[0])
+var y = c[2...4]
+print(c)
+print(y)
